7da528d9cdf184d09fd1e417360d53ce017c2e27
Andrew Hunter <andrew@logicalshift.co.uk>
2015-06-26 19:16:26 +0100

    Want a unification state object to make it possible to unify variables

---
+++ LogicalShift.Reason/Api/IUnificationState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LogicalShift.Reason.Api
+{
+    /// <summary>
+    /// Represents a possible unification state
+    /// </summary>
+    public interface IUnificationState
+    {
+        /// <summary>
+        /// True if the state is consistent, false if it's inconsistent
+        /// </summary>
+        bool Consistent { get; }
+    }
+}
