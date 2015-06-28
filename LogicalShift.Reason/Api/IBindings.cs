fa8f5b73c1f992d84f103aa24c3d23130e448aae
Andrew Hunter <andrew@logicalshift.co.uk>
2015-06-28 17:09:06 +0100

    Add a IBindings interface to represent variables which have been bound to things

---
+++ LogicalShift.Reason/Api/IBindings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LogicalShift.Reason.Api
+{
+    /// <summary>
+    /// Represents a series of variable bindings
+    /// </summary>
+    public interface IBindings
+    {
+        /// <summary>
+        /// Represents the unified result
+        /// </summary>
+        ILiteral Result { get; }
+
+        /// <summary>
+        /// The list of variables that were bound
+        /// </summary>
+        IEnumerable<ILiteral> Variables { get; }
+
+        /// <summary>
+        /// Finds what the specified variable was bound to
+        /// </summary>
+        ILiteral GetValueForVariable(ILiteral variable);
+    }
+}
