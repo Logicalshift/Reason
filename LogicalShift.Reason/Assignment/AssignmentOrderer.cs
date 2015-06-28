79216c14032196d68e83b5d21921e3b28a435e7b
Andrew Hunter <andrew@logicalshift.co.uk>
2015-06-28 12:14:29 +0100

    Add a class that orders assignments so that subterms come before terms

---
+++ LogicalShift.Reason/Assignment/AssignmentOrderer.cs
@@ -0,0 +1,54 @@
+﻿using LogicalShift.Reason.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LogicalShift.Reason.Assignment
+{
+    /// <summary>
+    /// Orders sets of assignments
+    /// </summary>
+    public static class AssignmentOrderer
+    {
+        /// <summary>
+        /// Returns a list of assignments ordered so that subterms come before terms (this is the order that they
+        /// should be built in order to compile a query)
+        /// </summary>
+        public static IEnumerable<IAssignmentLiteral> OrderTermsAfterSubterms(this IEnumerable<IAssignmentLiteral> list)
+        {
+            // Terms that have been returned
+            var seenTerms = new HashSet<ILiteral>();
+
+            // Quick lookup for the term with a particular variable
+            var termForVariable = list.ToDictionary(term => term.Variable);
+
+            // Variabes left to process
+            var toProcess = new Stack<ILiteral>(termForVariable.Keys);
+
+            while (toProcess.Any())
+            {
+                var candidate = toProcess.Pop();
+
+                // Don't return a term more than once
+                if (seenTerms.Contains(candidate)) continue;
+
+                var assignment = termForVariable[candidate];
+                if (assignment.Value.Dependencies.All(seenTerms.Contains))
+                {
+                    // All the terms for this assignment have been seen
+                    yield return assignment;
+                    seenTerms.Add(candidate);
+                }
+                else
+                {
+                    // Return the dependencies first
+                    toProcess.Push(candidate);
+                    foreach (var dep in assignment.Value.Dependencies)
+                    {
+                        toProcess.Push(dep);
+                    }
+                }
+            }
+        }
+    }
+}
