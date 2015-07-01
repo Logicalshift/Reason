6265999c7f90176a65f9660fb4387c9a934f3d76
Andrew Hunter <andrew@logicalshift.co.uk>
2015-07-01 23:01:09 +0100

    Instead of using a literal interpretation of the WAM heap, we could use a .NET representation (which should be faster, allow us to depend on .NET's  garbage collection and make it easier to write solvers that interoperate)

---
+++ LogicalShift.Reason/Api/IReferenceLiteral.cs
@@ -0,0 +1,35 @@
+﻿namespace LogicalShift.Reason.Api
+{
+    /// <summary>
+    /// Represents either a reference to a literal, or a literal term broken down into parameters
+    /// </summary>
+    public interface IReferenceLiteral
+    {
+        /// <summary>
+        /// null if this is just a reference to another literal, otherwise a literal representing the name of the term
+        /// </summary>
+        ILiteral Term { get; }
+
+        /// <summary>
+        /// What this is a reference to, provided that Term is null. If term is non-null, this is a reference to the first
+        /// argument. This can also be set to be self-referential to indicate that this reference should be treated as
+        /// an unbound variable.
+        /// </summary>
+        IReferenceLiteral Reference { get; }
+
+        /// <summary>
+        /// For items that are an argument to a term, this is the pointer to the next argument
+        /// </summary>
+        IReferenceLiteral NextArgument { get; }
+
+        /// <summary>
+        /// Sets the value of Term and Reference to the same values as the supplied reference literal
+        /// </summary>
+        void SetTo(IReferenceLiteral value);
+
+        /// <summary>
+        /// Freezes this reference into a normal literal
+        /// </summary>
+        ILiteral Freeze { get; }
+    }
+}
