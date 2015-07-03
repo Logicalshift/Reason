cffabec461415b540c57ae3494b272492adb6a39
Andrew Hunter <andrew@logicalshift.co.uk>
2015-07-03 08:53:23 +0100

    Add an empty single clause solver; we'll eventually use this as the basis of the simple 'interpreted' solver

---
+++ LogicalShift.Reason/Solvers/SimpleSingleClauseSolver.cs
@@ -0,0 +1,42 @@
+﻿using LogicalShift.Reason.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LogicalShift.Reason.Solvers
+{
+    /// <summary>
+    /// Represents a solver that can solve a single clause
+    /// </summary>
+    public class SimpleSingleClauseSolver : ISolver
+    {
+        /// <summary>
+        /// The clause that this will solve
+        /// </summary>
+        private readonly IClause _clause;
+
+        /// <summary>
+        /// The solver that will be used for subclauses
+        /// </summary>
+        private readonly ISolver _subclauseSolver;
+
+        public SimpleSingleClauseSolver(IClause clause, ISolver subclauseSolver)
+        {
+            if (clause == null) throw new ArgumentNullException("clause");
+            if (subclauseSolver == null) throw new ArgumentNullException("subclauseSolver");
+
+            _clause = clause;
+            _subclauseSolver = subclauseSolver;
+        }
+
+        public System.Threading.Tasks.Task<IQueryResult> Solve(IEnumerable<ILiteral> goals)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Func<bool> Call(ILiteral predicate, params IReferenceLiteral[] arguments)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
