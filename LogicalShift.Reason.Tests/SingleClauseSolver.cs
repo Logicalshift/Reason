8d5058d0ae9ab084fa6369092d716d5db213dc9e
Andrew Hunter <andrew@logicalshift.co.uk>
2015-07-04 16:10:29 +0100

    Add a simple test of the new solver

---
+++ LogicalShift.Reason.Tests/SingleClauseSolver.cs
@@ -0,0 +1,22 @@
+﻿using LogicalShift.Reason.Solvers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LogicalShift.Reason.Tests
+{
+    [TestFixture]
+    public class SingleClauseSolver
+    {
+        [Test]
+        public void SolveAsSelf()
+        {
+            var a = Literal.NewAtom();
+            var clause = Clause.Always(a);
+            var solver = new SimpleSingleClauseSolver(clause, new NothingSolver());
+
+            Assert.IsTrue(solver.Query(a)());
+        }
+    }
+}
---
+++ LogicalShift.Reason/Solvers/NothingSolver.cs
@@ -0,0 +1,22 @@
+﻿using LogicalShift.Reason.Api;
+using System;
+using System.Threading.Tasks;
+
+namespace LogicalShift.Reason.Solvers
+{
+    /// <summary>
+    /// Solver that always returns false
+    /// </summary>
+    public class NothingSolver : ISolver
+    {
+        public Task<IQueryResult> Solve(System.Collections.Generic.IEnumerable<ILiteral> goals)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Func<bool> Call(ILiteral predicate, params IReferenceLiteral[] arguments)
+        {
+            return () => false;
+        }
+    }
+}
