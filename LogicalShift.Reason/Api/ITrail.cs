eb342bfa854faef053faf831103c516259b2ae83
Andrew Hunter <andrew@logicalshift.co.uk>
2015-07-05 15:53:33 +0100

    Add an interface to represent the trail

---
+++ LogicalShift.Reason/Api/ITrail.cs
@@ -0,0 +1,18 @@
+﻿namespace LogicalShift.Reason.Api
+{
+    /// <summary>
+    /// A trail is use to backtrack on an operation by unbinding references
+    /// </summary>
+    public interface ITrail
+    {
+        /// <summary>
+        /// Records a reference that should be reset to unbound when this trail is reset
+        /// </summary>
+        void Record(IReferenceLiteral reference);
+
+        /// <summary>
+        /// Resets all of the recorded references back to being unbound
+        /// </summary>
+        void Reset();
+    }
+}
