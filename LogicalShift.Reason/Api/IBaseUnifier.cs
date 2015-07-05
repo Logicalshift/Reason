3e5a25f46d67e0778c3893c37af1e48abb44ddbd
Andrew Hunter <andrew@logicalshift.co.uk>
2015-07-05 12:02:23 +0100

    Update some documentation

--- LogicalShift.Reason/Api/IBaseUnifier.cs
+++ LogicalShift.Reason/Api/IBaseUnifier.cs
@@ -8,10 +8,15 @@
         /// <summary>
         /// Returns true if the variable identified has been used by SetVariable or UnifyVariable before
         /// </summary>
+        /// <remarks>
+        /// A variable is used if any variable bound to the same index has been encountered by an operation before.
+        /// </remarks>
         bool HasVariable(ILiteral variable);
 
         /// <summary>
-        /// Binds a variable index to a name
+        /// Binds a variable index to a name. Multiple variable names can be bound to the same
+        /// index if necessary (they will all become aliases for the same storage location).
+        /// Variable names can't be used before they are bound.
         /// </summary>
         void BindVariable(int index, ILiteral variable);
 
