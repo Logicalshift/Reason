f78ba635fa213566873144eedf6e39b39c2c0a2c
Andrew Hunter <andrew@logicalshift.co.uk>
2015-07-05 15:58:50 +0100

    Add a basic implementation of a trail

---
+++ LogicalShift.Reason/Solvers/BasicTrail.cs
@@ -0,0 +1,32 @@
+﻿using LogicalShift.Reason.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LogicalShift.Reason.Solvers
+{
+    /// <summary>
+    /// Simplest possible implementation of a trail
+    /// </summary>
+    public class BasicTrail : ITrail
+    {
+        /// <summary>
+        /// Items to reset when this trail is reset
+        /// </summary>
+        private readonly List<IReferenceLiteral> _toReset = new List<IReferenceLiteral>();
+
+        public void Record(IReferenceLiteral reference)
+        {
+            _toReset.Add(reference);
+        }
+
+        public void Reset()
+        {
+            foreach (var val in _toReset)
+            {
+                val.Unbind();
+            }
+            _toReset.Clear();
+        }
+    }
+}
