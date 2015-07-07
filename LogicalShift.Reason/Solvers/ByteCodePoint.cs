aebd7fc42cbfd0c2765c8976e74a094d98384416
Andrew Hunter <andrew@logicalshift.co.uk>
2015-07-07 21:31:53 +0100

    Add a structure to represent a bytecode instruction

---
+++ LogicalShift.Reason/Solvers/ByteCodePoint.cs
@@ -0,0 +1,30 @@
+﻿namespace LogicalShift.Reason.Solvers
+{
+    /// <summary>
+    /// Represents a code point in a bytecode solver's program
+    /// </summary>
+    public struct ByteCodePoint
+    {
+        public ByteCodePoint(Operation op, int arg1 = 0, int arg2 = 0)
+        {
+            Op = op;
+            Arg1 = arg1;
+            Arg2 = arg2;
+        }
+
+        /// <summary>
+        /// The operation to execute
+        /// </summary>
+        public readonly Operation Op;
+
+        /// <summary>
+        /// The first argument (generally a variable, but can be an offset, a length or a reference to a literal)
+        /// </summary>
+        public readonly int Arg1;
+
+        /// <summary>
+        /// The second argument (generally a variable)
+        /// </summary>
+        public readonly int Arg2;
+    }
+}
