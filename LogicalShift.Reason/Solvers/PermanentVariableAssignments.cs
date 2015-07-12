0b8a2bba87004aed98b8de28e9cae02fadf70e7d
Andrew Hunter <andrew@logicalshift.co.uk>
2015-07-12 14:57:00 +0100

    Add a way to get the list of permanent variables from a set of assignments representing a clause

--- LogicalShift.Reason/Solvers/PermanentVariableAssignments.cs
+++ LogicalShift.Reason/Solvers/PermanentVariableAssignments.cs
@@ -34,5 +34,47 @@ namespace LogicalShift.Reason.Solvers
             // Result is arguments followed by the list with the permanent variables first
             return arguments.Concat(permanentFirst);
         }
+
+        /// <summary>
+        /// Returns the names of the variables that get assigned in a list of assignments
+        /// </summary>
+        public static IEnumerable<ILiteral> VariablesAssigned(this IEnumerable<IAssignmentLiteral> assignments)
+        {
+            return assignments
+                .Where(assign => assign.Value.UnificationKey == null)
+                .Select(assign => assign.Value);
+        }
+
+        /// <summary>
+        /// Given a list of predicate assignment lists (representing the assignments for the initial predicate and
+        /// the clauses, in order), finds the names of the 'permanent' variables: that is, the list of variables
+        /// that are used in more than one clause.
+        /// </summary>
+        public static IEnumerable<ILiteral> PermanentVariables(this IEnumerable<PredicateAssignmentList> assignmentLists)
+        {
+            // The predicate and the first clause don't have a call between them, so variables used in both places 
+            // aren't considered to be permanent
+            var usedVariables = new HashSet<ILiteral>();
+            var permanentVariabels = new HashSet<ILiteral>();
+            int pos = 0;
+
+            foreach (var assignmentList in assignmentLists)
+            {
+                var assignmentVariables = VariablesAssigned(assignmentList.Assignments).ToArray();
+
+                // After the predicate (pos = 0) and first clause (pos = 1), re-used variables need to be marked as permanent
+                if (pos > 1)
+                {
+                    permanentVariabels.UnionWith(assignmentVariables.Where(variable => usedVariables.Contains(variable)));
+                }
+
+                // Mark this set of variables as used
+                usedVariables.UnionWith(assignmentVariables);
+
+                ++pos;
+            }
+
+            return permanentVariabels;
+        }
     }
 }
