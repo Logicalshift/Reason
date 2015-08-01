0ad38999d75a7b209290650917e6341aafc8f743
Andrew Hunter <andrew@logicalshift.co.uk>
2015-08-01 16:18:24 +0100

    Add implementations of GetStructure and UnifyVariable.

--- LogicalShift.Reason/Solvers/ByteCodeExecutor.cs
+++ LogicalShift.Reason/Solvers/ByteCodeExecutor.cs
@@ -34,6 +34,21 @@ namespace LogicalShift.Reason.Solvers
         /// </summary>
         private SimpleReference[] _registers;
 
+        /// <summary>
+        /// True if we're in 'write mode'
+        /// </summary>
+        bool _writeMode;
+
+        /// <summary>
+        /// Pointer to the last built argument
+        /// </summary>
+        IReferenceLiteral _lastArgument;
+
+        /// <summary>
+        /// Pointer to the next value within the current structure
+        /// </summary>
+        IReferenceLiteral _structurePtr;
+
         public ByteCodeExecutor(ByteCodePoint[] program, ILiteral[] literals, int maxVariableIndex)
         {
             if (program == null) throw new ArgumentNullException(nameof(program));
@@ -68,6 +83,9 @@ namespace LogicalShift.Reason.Solvers
                     break;
 
                 case Operation.GetStructure:
+                    GetStructure(_program[address].Literal, _program[address].Arg1, _program[address].Arg2);
+                    break;
+
                 case Operation.GetVariable:
                 case Operation.GetValue:
                 case Operation.PutStructure:
@@ -75,7 +93,12 @@ namespace LogicalShift.Reason.Solvers
                 case Operation.PutValue:
                 case Operation.SetVariable:
                 case Operation.SetValue:
+                    throw new NotImplementedException("Opcode not implemented yet");
+
                 case Operation.UnifyVariable:
+                    UnifyVariable(_program[address].Arg1);
+                    break;
+
                 case Operation.UnifyValue:
                 case Operation.Allocate:
                 case Operation.Deallocate:
@@ -92,6 +115,86 @@ namespace LogicalShift.Reason.Solvers
                 default:
                     throw new NotImplementedException("Unknown opcode");
             }
+        }
+
+        /// <summary>
+        /// Implements the GetStrucutre opcode
+        /// </summary>
+        private bool GetStructure(int literal, int variable, int termLength)
+        {
+            var termName = _literals[literal];
+
+            // Get the dereferenced address of the variable
+            var heapValue = _registers[variable].Dereference();
+
+            // Action depends on what's at that address
+            if (heapValue.IsVariable())
+            {
+                // Variable is an unbound ref cell: bind it to a new value that we create
+                ArgumentReference firstArgument = null;
+
+                for (int argNum = 0; argNum < termLength; ++argNum)
+                {
+                    var newArgument = new ArgumentReference(null, firstArgument);
+                    firstArgument = newArgument;
+                }
+
+                var newStructure = new SimpleReference(termName, firstArgument);
+                _lastArgument = firstArgument;
+                _structurePtr = firstArgument;
+
+                Bind(heapValue, newStructure);
+                _writeMode = true;
+            }
+            else if (!heapValue.IsReference())
+            {
+                if (Equals(heapValue.Term, termName))
+                {
+                    // Set the structure pointer, and use read mode
+                    _structurePtr = heapValue.Reference;
+                    _writeMode = false;
+                }
+                else
+                {
+                    // Structure doesn't match; fail
+                    return false;
+                }
+            }
+            else
+            {
+                // Fail
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Unifies a variable
+        /// </summary>
+        private void UnifyVariable(int variable)
+        {
+            if (!_writeMode)
+            {
+                // Just read the value of the variable
+                _registers[variable].SetTo(_structurePtr);
+            }
+            else
+            {
+                // Write the value of the variable
+                _registers[variable].SetTo(_lastArgument);
+                _lastArgument = _lastArgument.NextArgument;
+            }
+
+            _structurePtr = _structurePtr.NextArgument;
+        }
+
+        /// <summary>
+        /// Binds one value to anoteher
+        /// </summary>
+        private void Bind(IReferenceLiteral target, IReferenceLiteral value)
+        {
+            target.SetTo(value);
         }
     }
 }
