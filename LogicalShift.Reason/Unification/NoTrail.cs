452dcd51f5ebe3108edd26afcb4bde7b600a314f
Andrew Hunter <andrew@logicalshift.co.uk>
2015-08-01 16:31:51 +0100

    Add a trail representing 'no backtracking supported'

---
+++ LogicalShift.Reason/Unification/NoTrail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using LogicalShift.Reason.Api;
+
+namespace LogicalShift.Reason.Unification
+{
+    /// <summary>
+    /// Trail used when no back tracking is supported
+    /// </summary>
+    public class NoTrail : ITrail
+    {
+        public void Record(IReferenceLiteral reference)
+        {
+        }
+
+        public void Reset()
+        {
+            throw new NotSupportedException("Backtracking is not supported");
+        }
+    }
+}
