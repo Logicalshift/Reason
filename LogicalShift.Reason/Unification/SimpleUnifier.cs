a8bca5becc3b9c120aeadab6c6435d1caae87b72
Andrew Hunter <andrew@logicalshift.co.uk>
2015-08-01 16:28:24 +0100

    Move the 'unify' function into a static class of its own

--- LogicalShift.Reason/Unification/SimpleUnifier.cs
+++ LogicalShift.Reason/Unification/SimpleUnifier.cs
@@ -333,57 +333,7 @@ namespace LogicalShift.Reason.Unification
         /// </summary>
         private bool Unify(IReferenceLiteral address1, IReferenceLiteral address2)
         {
-            var unifyStack = new Stack<IReferenceLiteral>();
-
-            // Push the addresses that we're going to unify
-            unifyStack.Push(address1);
-            unifyStack.Push(address2);
-
-            // Iterate until the stack is empty
-            while (unifyStack.Count > 0)
-            {
-                // Deref the values on the stack
-                var value1 = unifyStack.Pop().Dereference();
-                var value2 = unifyStack.Pop().Dereference();
-
-                if (!ReferenceEquals(value1, value2))
-                {
-                    if (value1.IsReference())
-                    {
-                        // Bind references
-                        Bind(value1, value2);
-                    }
-                    else if (value2.IsReference())
-                    {
-                        Bind(value2, value1);
-                    }
-                    else
-                    {
-                        if (Equals(value1.Term, value2.Term))
-                        {
-                            // Process the rest of the structure
-                            var structurePos1 = value1.Reference;
-                            var structurePos2 = value2.Reference;
-
-                            while (structurePos1 != null)
-                            {
-                                unifyStack.Push(structurePos1);
-                                unifyStack.Push(structurePos2);
-
-                                structurePos1 = structurePos1.NextArgument;
-                                structurePos2 = structurePos2.NextArgument;
-                            }
-                        }
-                        else
-                        {
-                            // Structures do not match: fail
-                            return false;
-                        }
-                    }
-                }
-            }
-
-            return true;
+            return address1.Unify(address2, _trails.Peek());
         }
 
         public IQueryUnifier QueryUnifier
---
+++ LogicalShift.Reason/Unification/UnifyReferences.cs
@@ -0,0 +1,72 @@
+﻿using LogicalShift.Reason.Api;
+using System.Collections.Generic;
+
+namespace LogicalShift.Reason.Unification
+{
+    /// <summary>
+    /// Provides support for unifying reference literals
+    /// </summary>
+    public static class UnifyReferences
+    {
+
+        /// <summary>
+        /// Unifies a value on the heap
+        /// </summary>
+        public static bool Unify(this IReferenceLiteral address1, IReferenceLiteral address2, ITrail trail)
+        {
+            var unifyStack = new Stack<IReferenceLiteral>();
+
+            // Push the addresses that we're going to unify
+            unifyStack.Push(address1);
+            unifyStack.Push(address2);
+
+            // Iterate until the stack is empty
+            while (unifyStack.Count > 0)
+            {
+                // Deref the values on the stack
+                var value1 = unifyStack.Pop().Dereference();
+                var value2 = unifyStack.Pop().Dereference();
+
+                if (!ReferenceEquals(value1, value2))
+                {
+                    if (value1.IsReference())
+                    {
+                        // Bind references
+                        trail.Record(value1);
+                        value1.SetTo(value2);
+                    }
+                    else if (value2.IsReference())
+                    {
+                        trail.Record(value2);
+                        value2.SetTo(value1);
+                    }
+                    else
+                    {
+                        if (Equals(value1.Term, value2.Term))
+                        {
+                            // Process the rest of the structure
+                            var structurePos1 = value1.Reference;
+                            var structurePos2 = value2.Reference;
+
+                            while (structurePos1 != null)
+                            {
+                                unifyStack.Push(structurePos1);
+                                unifyStack.Push(structurePos2);
+
+                                structurePos1 = structurePos1.NextArgument;
+                                structurePos2 = structurePos2.NextArgument;
+                            }
+                        }
+                        else
+                        {
+                            // Structures do not match: fail
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
